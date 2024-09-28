4ae50a158a07254aad4fe1377192d838c1b857f5
martelleur <joel.martelleur@gmail.com>
2024-09-28 15:06:15 +0200

    Update current course page to show selected activities and modules.

--- src/pages/CurrentCoursePage/ModulePanel.tsx
+++ src/pages/CurrentCoursePage/ModulePanel.tsx
@@ -1,44 +1,41 @@
+
 import { ReactElement } from "react";
 
 import { H, IconContainer, P, TextColor } from "@/components";
 import { FaChevronDown, FaChevronRight } from "react-icons/fa";
-import { type ModuleDto } from "@/apiGenerated";
+import type { ActivityDto, ModuleDto } from "@/apiGenerated";
+import { SidebarPanel } from "./SidebarPanel";
 
 interface Props {
   module: ModuleDto;
   open: boolean;
   toggleOpen: () => void;
+  selectActivity: (activity: ActivityDto) => void;
 }
 
 export const ModulePanel: React.FC<Props> = ({
   module,
   open,
-  toggleOpen
+  toggleOpen,
+  selectActivity
 }): ReactElement => {
+  const handleSelectActivity = (
+    activity: ActivityDto,
+    e:  React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    e.stopPropagation();
+    selectActivity(activity);
+  };
+
   return (
-    <article key={module.id}
-      className={`border-t  ${open && "bg-indigo-950"}
-      cursor-pointer hover:bg-indigo-950 pb-1`}
-      onClick={toggleOpen}>
-      <div className="flex justify-between items-center p-4 overflow-hidden">
-        <H size={4}>{module.name}</H>
-        {!open
-          ?
-          <IconContainer
-            className="text-white size-4 cursor-pointer"><FaChevronRight />
-          </IconContainer>
-          : <IconContainer
-            className="text-white size-4 cursor-pointer"><FaChevronDown />
-          </IconContainer>
-        }
-      </div>
+    <SidebarPanel title={`${module.name}`} open={open} toggleOpen={toggleOpen}>
       {open && module.activities != null && module.activities.map(activity => (
         <div key={activity.id}
+          onClick={e => { handleSelectActivity(activity, e); }}
           className="flex justify-start items-start p-2 m-2 
           cursor-pointe">
           <P color={TextColor.MEDIUM}>🚀 {activity.description}</P>
         </div>
       ))}
-    </article>
+    </SidebarPanel>
   );
 };
