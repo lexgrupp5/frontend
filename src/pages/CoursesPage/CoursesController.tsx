d91292a50341e23837b98414c05786619ec40acf
martelleur <joel.martelleur@gmail.com>
2024-10-01 13:13:16 +0200

    Fix form ui after demo feedback

--- src/pages/CoursesPage/CoursesController.tsx
+++ src/pages/CoursesPage/CoursesController.tsx
@@ -18,19 +18,25 @@ export const CoursesController = (): ReactElement => {
     <article className="flex flex-col-reverse pt-16 justify-center items-center
       gap-6 px-6
       sm:flex-row">
-      <SearchFilterInput />
-      {authContext.isTeacher() && <UnstyledButton
-        className="display flex justify-center items-center
-        w-full py-2 pl-4 pr-2 
-        rounded-t-lg rounded-b-lg 
-        bg-white
-        sm:w-auto"
-        onPress={navigateToSelectedCoursePage}>
-        <P color={TextColor.DARK}>Create new course</P>
-        <IconContainer className="size-10 p-2 border-none focus:outline-none">
-          <MdAssignmentAdd />
-        </IconContainer>
-      </UnstyledButton>}
+      <div className="rounded-lg hover:outline
+         outline-offset-2 outline-gray-300">
+        <SearchFilterInput />
+      </div>
+      {authContext.isTeacher() && <div className="rounded-lg hover:outline
+         outline-offset-2 outline-gray-300">  
+        <UnstyledButton
+          className="display flex justify-center items-center
+          w-full py-2 pl-4 pr-2 
+          rounded-t-lg rounded-b-lg 
+          bg-white
+          sm:w-auto"
+          onPress={navigateToSelectedCoursePage}>
+          <P color={TextColor.DARK}>Create new course</P>
+          <IconContainer className="size-10 p-2 border-none focus:outline-none">
+            <MdAssignmentAdd />
+          </IconContainer>
+        </UnstyledButton>
+      </div>}
     </article>
   );
 };
