683559dce0919ba05a68e1b43ec8cf14b57eda8e
martelleur <joel.martelleur@gmail.com>
2024-10-01 02:03:54 +0200

    Update search items backgrounds.

--- src/services/env.ts
+++ src/services/env.ts
@@ -1,7 +1,7 @@
 import { isDevelopment } from "@/config";
 
 export function getDefaultUsername() {
-  return isDevelopment() ? "Jessica27" : "";
+  return isDevelopment() ? "Gustav_Ali" : "";
 }
 
 export function getDefaultPwd() {
