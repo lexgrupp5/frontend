a515a1c30a1316ef6dff12c6754cd7438b89df95
RikiRhen <riki.rhen@live.se>
2024-10-02 12:39:06 +0200

    Module status codes + ModuleId getting dynamically fetched

--- src/providers/StudentPageProvider.tsx
+++ src/providers/StudentPageProvider.tsx
@@ -1,32 +1,68 @@
-import { useEffect, ReactElement } from "react";
+import { useEffect, useState, ReactElement } from "react";
 import { Outlet } from "react-router-dom";
 
-import { api } from "@/api";
+import { api, IModuleDto } from "@/api";
 import { IStudentPageContext } from "@/contexts";
 import { useApi } from "@/hooks/useApi";
 
 export const StudentPageProvider = (): ReactElement => {
-    //PLACEHOLDER ID, SHOULD BE FETCHED FROM THE LOGGED IN USERS REGISTERED COURSE
-    const courseId = 3;
     const { pending: coursePending, error: courseError, data: courseData, makeAuthRequest: fetchCourseData, clearError: clearCourseError } = useApi(api.getCourse);
     const { pending: modulePending, error: moduleError, data: moduleData, makeAuthRequest: fetchModuleData, clearError: clearModuleError } = useApi(api.modulesAll);
     const { pending: activityPending, error: activityError, data: activityData, makeAuthRequest: fetchActivityData, clearError: clearActivityError } = useApi(api.activities);
     const { pending: participantsPending, error: participantsError, data: participantsData, makeAuthRequest: fetchParticipantData, clearError: clearParticipantError } = useApi(api.course);
-    //PLACEHOLDER ID, SHOULD BE FETCHED FROM THE CURRENT RELEVANT MODULE
-    const moduleId = 7;
+    //STARTS OFF AS -1, GETS ASSIGNED AFTER MODULES HAVE BEEN FETCHED FROM API OR IS LEFT -1 IF NO MODULES WERE FOUND
+    const [moduleId, setModuleId] = useState<number | null>(-1)
+
+    //PLACEHOLDER ID, SHOULD BE FETCHED FROM THE LOGGED IN USERS REGISTERED COURSE
+    //const courseId = User.CourseId
+    const courseId = 3;
+
+    const setCurrentModule = async (modules: IModuleDto[]) => {
+        const dateNow = new Date();
+
+        if (!modules || modules.length === 0) {
+            setModuleId(-1);
+            return -1;
+        }
+
+        if (dateNow > new Date(modules[modules.length - 1].endDate!)) {
+            setModuleId(modules[modules.length - 1].id!);
+            return modules[modules.length - 1].id!;
+        }
+
+        if (dateNow < new Date(modules[0].startDate!)) {
+            setModuleId(modules[0].id!);
+            return modules[0].id!;
+        }
+
+        const activeModule = modules.find(
+            (module) => dateNow >= new Date(module.startDate!) && dateNow <= new Date(module.endDate!)
+        );
+
+        const activeModuleId = activeModule?.id! ?? -1;
+        setModuleId(activeModuleId);
+        return activeModuleId;
+    }
+
 
     useEffect(() => {
         (async () => {
             try {
                 await fetchCourseData(courseId);
-                await fetchModuleData(courseId);
-                await fetchActivityData(moduleId!);
-                await fetchParticipantData(courseId);
+                const fetchedModules = await fetchModuleData(courseId);
+                let newModuleId = moduleId;
+                if (moduleId === -1) {
+                    newModuleId = await setCurrentModule(fetchedModules!)
+                }
+                if (newModuleId !== -1) {
+                    await fetchActivityData(newModuleId!);
+                    await fetchParticipantData(courseId);
+                }
             } catch (e) {
                 console.log(e);
             }
         })();
-    }, []);
+    }, [moduleId]);
 
     const constructStudentPageContext = (): IStudentPageContext => ({
         course: courseData,
