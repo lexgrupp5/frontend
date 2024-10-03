6e2eb74124ab5ccc0c57e4b9c771dc5c4869adf3
RikiRhen <riki.rhen@live.se>
2024-10-03 14:40:42 +0200

    Backend endpoint needs fixing

--- src/pages/CurrentCoursePage/CourseSidebarBody.tsx
+++ src/pages/CurrentCoursePage/CourseSidebarBody.tsx
@@ -4,13 +4,15 @@ import { ReactElement, useState } from "react";
 import { ModuleDto, UserDto } from "@/apiGenerated";
 import { ModulePanel } from "./ModulePanel";
 import { CourseModal } from "./CourseModal";
+import { NewUserModal } from "./NewUserModal";
 import { CreateActivityForm } from "./CreateActivityForm";
+import { CreateUserForm } from "./CreateUserForm";
 import { useCurrentCourseContext } from "@/hooks";
 import { ParticipantsPanel } from "./ParticipantsPanel";
 
 interface Props {
   modules: ModuleDto[];
-  openPanels:  Record<string, boolean>;
+  openPanels: Record<string, boolean>;
   participants: UserDto[];
   toggleModulePanelOpen: (id: string, module: ModuleDto) => void;
   toggleParticipantPanelOpen: (id: string) => void;
@@ -26,9 +28,10 @@ export const CourseSidebarBody: React.FC<Props> = ({
   updateCacheTimestamp
 }): ReactElement => {
   const [createActivity, setCreateActivity] = useState(false);
+  const [createUser, setCreateUser] = useState(false);
   const { selectedModule, selectedCourse } = useCurrentCourseContext();
   const participantPanelId = "partcipantsPanel";
-  
+
   return (
     <>
       <div />
@@ -37,14 +40,19 @@ export const CourseSidebarBody: React.FC<Props> = ({
         onClose={() => { setCreateActivity(false); updateCacheTimestamp(); }}>
         <CreateActivityForm title={`Create activity
         ${selectedModule?.name ? `for ${selectedModule?.name}` : ""}
-        in course '${selectedCourse?.name}'`}/>
+        in course '${selectedCourse?.name}'`} />
       </CourseModal>}
+      {<NewUserModal
+        isOpen={createUser}
+        onClose={() => { setCreateUser(false); updateCacheTimestamp(); }}>
+        <CreateUserForm title={`Add new user`} />
+      </NewUserModal>}
       <ParticipantsPanel
         participants={participants}
         panelId={participantPanelId}
         open={openPanels[participantPanelId]}
         toggleOpen={toggleParticipantPanelOpen}
-        openCreateParticipant={() => {  }} />
+        openCreateParticipant={() => { setCreateUser(true); }} />
       {modules.map(module => {
         if (module.id == null) { return <></>; }
         return <div key={module.id}>
---
+++ src/pages/CurrentCoursePage/CreateUserForm.tsx
@@ -0,0 +1,82 @@
+import { FormEventHandler, ReactElement, useState } from "react";
+import { api, UserForCreationDto } from "@/api";
+import { H, Input, SubmitButton, TextColor } from "@/components";
+import { DefaultToastMessage } from "../SharedComponents";
+import { useApi, useCurrentCourseContext, useMessageContext } from "@/hooks";
+
+interface Props {
+    title?: string
+}
+
+export const CreateUserForm: React.FC<Props> = ({ title }): ReactElement => {
+    const createUser = useApi(api.userPOST);
+    const [name, setName] = useState("");
+    const [username, setUsername] = useState("");
+    const [email, setEmail] = useState("");
+    const msgContext = useMessageContext();
+
+    const submit: FormEventHandler<HTMLFormElement> = async (e) => {
+        e.preventDefault();
+        createUser.clearError();
+
+        // LÄGGER ENDAST TILL USER I DATABASEN. GER INTE USER EN KURS ÄN. USERFORCREATION DTO SAKNAR COURSE ID
+        const [err, result] = await createUser.makeAuthRequestWithErrorResponse(
+            new UserForCreationDto({
+                name,
+                username,
+                email
+            })
+        );
+
+        if (err === null && result !== null) {
+            msgContext.updateMessage(`User '${result.name}' has been created.`);
+            clearInputs();
+        } else {
+            msgContext.updateErrorMessage(`The user was not able to be created.`)
+        }
+    };
+
+    const clearInputs = () => {
+        setName("");
+        setUsername("");
+        setEmail("");
+    };
+
+    const handleCloseForm = () => {
+        createUser.clearError();
+    };
+
+
+    return (
+        <form onSubmit={submit}
+            className="w-full
+            bg-indigo-100
+            rounded-lg max-w-lg">
+            <DefaultToastMessage onClose={handleCloseForm} />
+            <H size={3} color={TextColor.DARK_X} className="mb-2">
+                {title}
+            </H>
+            <fieldset className="flex flex-col gap-6">
+                <Input
+                    type="text"
+                    label="Name"
+                    required
+                    value={name}
+                    onChange={e => { setName(e.target.value); }} />
+                <Input
+                    type="text"
+                    label="Username"
+                    required
+                    value={username}
+                    onChange={e => { setUsername(e.target.value); }} />
+                <Input
+                    type="text"
+                    label="E-mail"
+                    required
+                    value={email}
+                    onChange={e => { setEmail(e.target.value); }} />
+                <SubmitButton>Add</SubmitButton>
+            </fieldset>
+        </form>
+    );
+};
